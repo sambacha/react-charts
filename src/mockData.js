3faadab4a40e81019a86e2dca511c38d14214441
sam <sam@freighttrust.com>
2020-09-26 01:52:58 -0700

    gh-pages script

--- src/mockData.js
+++ src/mockData.js
@@ -1,76 +1,70 @@
 export const variables = [
-  
-  full_name: '211 - Bill of Lading',
-  id: 1,
-  type: 'X12',
-  version: 'C'
-},
 {
-  full_name: '4010',
-  id: 2,
-  type: 'X12',
-  version: 'C'
-},
-{
-  full_name: 'D.07A',
-  id: 3,
-  type: 'EDIFACT',
-  version: 'C'
-},
-{
-  full_name: '[TXID]',
-  id: 4,
-  type: 'vegetable',
-  version: 'C'
-},
-{
-  full_name: '[TXID]',
-  id: 5,
-  type: 'X12',
-  version: 'C'
-},
-{
-  full_name: '[TXID]',
-  id: 6,
-  type: 'X12',
-  version: 'hr'
-},
-{
-  full_name: '[TXID]',
-  id: 7,
-  type: 'X12',
-  version: 'hr'
-},
-{
-  full_name: '[TXID]',
-  id: 8,
-  type: 'X12',
-  version: 'hr'
-},
-{
-  full_name: '[TXID]',
-  id: 9,
-  type: 'X12',
-  version: 'hr'
-},
-{
-  full_name: '[TXID]',
-  id: 10,
-  is_meter: 0,
-  name: '[TXID]',
-  simulation_id: 9,
-  type: 'X12',
-  version: 'hr'
-},
-];
+		full_name: '211 - Bill of Lading',
+		id: 1,
+		type: 'X12',
+		version: 'C'
+	},
+	{
+		full_name: '4010',
+		id: 2,
+		type: 'X12',
+		version: 'C'
+	}, {
+		full_name: 'D.07A',
+		id: 3,
+		type: 'EDIFACT',
+		version: 'C'
+	}, {
+		full_name: '[TXID]',
+		id: 4,
+		type: 'vegetable',
+		version: 'C'
+	}, {
+		full_name: '[TXID]',
+		id: 5,
+		type: 'X12',
+		version: 'C'
+	}, {
+		full_name: '[TXID]',
+		id: 6,
+		type: 'X12',
+		version: 'hr'
+	}, {
+		full_name: '[TXID]',
+		id: 7,
+		type: 'X12',
+		version: 'hr'
+	}, {
+		full_name: '[TXID]',
+		id: 8,
+		type: 'X12',
+		version: 'hr'
+	}, {
+		full_name: '[TXID]',
+		id: 9,
+		type: 'X12',
+		version: 'hr'
+	}, {
+		full_name: '[TXID]',
+		id: 10,
+		is_meter: 0,
+		name: '[TXID]',
+		simulation_id: 9,
+		type: 'X12',
+		version: 'hr'
+  }
+ ];
 
 export const chartPie = {
-  chart_data: '',
-  chart_name: 'tony pie',
-  created_at: '2020-09-18T15:27:19.000000Z',
-  id: 9,
-  options: { variables: [] },
-  variables: [],
-  type: 'pie',
-  updated_at: '2020-09-18T15:27:19.000000Z',
+	chart_data: '',
+	chart_name: 'tony pie',
+	created_at: '2020-09-18T15:27:19.000000Z',
+	id: 9,
+	options: {
+		variables: []
+	},
+	variables: [],
+	type: 'pie',
+	updated_at: '2020-09-18T15:27:19.000000Z'
 };
