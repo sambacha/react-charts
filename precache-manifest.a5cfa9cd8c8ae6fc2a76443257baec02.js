9ac2199bf2806abfa8be2215efe6ca0a26c9e2dd
sam <sam@freighttrust.com>
2020-09-26 01:53:28 -0700

    Updates

---
+++ precache-manifest.a5cfa9cd8c8ae6fc2a76443257baec02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230ed7137e8e8c144bb1fac7475da61a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63cebfa83afebfaa08a",
+    "url": "/static/css/2.1c136e97.chunk.css"
+  },
+  {
+    "revision": "0057716ee2ad8051fc81",
+    "url": "/static/css/main.24639203.chunk.css"
+  },
+  {
+    "revision": "f63cebfa83afebfaa08a",
+    "url": "/static/js/2.71efe87a.chunk.js"
+  },
+  {
+    "revision": "29beacc70c8d16054a612155e956d9b1",
+    "url": "/static/js/2.71efe87a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0057716ee2ad8051fc81",
+    "url": "/static/js/main.4d00256b.chunk.js"
+  },
+  {
+    "revision": "351b4785ea350d476240",
+    "url": "/static/js/runtime-main.49707207.js"
+  }
+]);
